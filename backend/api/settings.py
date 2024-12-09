f04e0d67686990c2a6613bfe1e7957013328c0db
Pati <bhagyaraju.pati@zoetis.com>
2024-12-09 16:06:09 +0530

    static folder added now

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Your local static files
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
